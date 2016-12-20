33f1368650d338959597ed4a129a95ed0f99ed14
Nor Shazwan Shah Nor Hisham Shah <shazwan.hisham@mimos.local>
2016-12-20 15:53:28 +0800

    added users class

---
+++ server/utils/users.js
@@ -0,0 +1,55 @@
+[{
+  id: '/#sdfsd'
+}]
+
+//add user{id,name, roomName}
+//removeUser(id)
+//getUser
+//getUserList(room)
+
+class Users {
+  constructor () {
+    this.users = [];
+  }
+  addUser (id, name, room) {
+    var user = {id, name, room};
+    this.users.push(user);
+    return user;
+  }
+  removeUser (id) {
+    var user = this.getUser(id);
+
+    if (user) {
+      this.users = this.users.filter((user) => user.id !== id);
+    }
+
+    return user;
+  }
+  getUser (id) {
+    return this.users.filter((user) => user.id === id)[0];
+  }
+  getUserList (room) {
+    var users = this.users.filter((user) => user.room === room);
+    var namesArray = users.map((user) => user.name);
+
+    return namesArray;
+  }
+}
+
+module.exports = {Users};
+
+// class Person {
+//   constructor (name, age) {
+//     this.name = name;
+//     this.age = age;
+//   }
+//   getUserDescription () {
+//     return `${this.name} is ${this.age} year(s) old`;
+//   }
+// }
+//
+// var me = new Person('shazwan', 25);
+// console.log('this.name', me.name);
+// console.log('this.age', me.age);
+// var desription = me.getUserDescription();
+// console.log(desription);
